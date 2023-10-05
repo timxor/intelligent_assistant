ddb3d7fa3338d8e027c5a352f51090ca73e9b19f
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2023-10-05 16:28:23 -0500

    :bowtie: [auto generated] via tim's deploy_script.sh

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+app.use(bodyParser.json());
+
+
+/* main page
+***************************/
+
+
+// Serve static files like CSS, JS, and images
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the index.html file for the root path
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+app.post('/ask', (req, res) => {
+    const userQuery = req.body.query;
+    
+    // Here, you can add your logic to process the query and generate a response.
+    // For this example, I'll just echo back the query.
+    const response = `You asked: ${userQuery}`;
+    
+    res.json({ response });
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
